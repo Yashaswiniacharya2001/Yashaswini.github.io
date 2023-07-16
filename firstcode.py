04f3366bbd1c03e79d866df4fb4853f831bec068
Yashaswiniacharya2001 <139705749+Yashaswiniacharya2001@users.noreply.github.com>
2023-07-16 22:21:23 +0530

    Add files via upload

---
+++ firstcode.py
@@ -0,0 +1,14 @@
+#Scripting way:
+print("Hello world")
+
+#procedural way
+def first():
+    print("Hello world")
+first()
+
+#object oriented style
+class Demo:
+    def disp(self):
+        print("Hello world")
+d=Demo()
+d.disp()
---
+++ practice.py
@@ -0,0 +1,7 @@
+li=[1,2,3,4,5]
+li[2]=10
+print(li)
+li.append(20)
+print(li)
+li.remove(20)
+print(li)
